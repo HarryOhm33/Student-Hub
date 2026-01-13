027e94a0aa48b0e8776ca94754c3947dbb412e86
Hari Om <hari333333om@gmail.com>
2026-01-13 14:15:35 +0530

    Added sameSite lzx

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -190,6 +190,7 @@ module.exports.login = async (req, res) => {
   res.cookie("autoKey", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
+    sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
 
